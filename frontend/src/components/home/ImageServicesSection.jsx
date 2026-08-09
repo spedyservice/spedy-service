7231dcd40ae499db6ccf65ce4e90814ae000cf61
spedyservice <283765478+spedyservice@users.noreply.github.com>
2026-08-09 21:24:48 +0530

    Restore original ImageServicesSection design

--- frontend/src/components/home/ImageServicesSection.jsx
+++ frontend/src/components/home/ImageServicesSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
-import wmImg from '../../assets/washingmachine.avif'
+import wmImg from '../../assets/washingmachine.png'
 import fridgeImg from '../../assets/refrigerator.png'
 import tvImg from '../../assets/tv repair.jpeg'
 import acImg from '../../assets/Ac repair.jpg'
